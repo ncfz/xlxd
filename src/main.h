369b9cfddee94ba12cffff2868116c0566232289
ncfz <bob8bob@qq.com>
2021-12-27 15:27:02 +0800

    Update main.h

--- src/main.h
+++ src/main.h
@@ -126,7 +126,7 @@
 #define YSF_DEFAULT_NODE_TX_FREQ        437000000                           // in Hz
 #define YSF_DEFAULT_NODE_RX_FREQ        437000000                           // in Hz
 #define YSF_AUTOLINK_ENABLE             0                                   // 1 = enable, 0 = disable auto-link
-#define YSF_AUTOLINK_MODULE             'B'                                 // module for client to auto-link to
+define YSF_AUTOLINK_MODULE             'B'                                 // module for client to auto-link to
 
 // G3 Terminal
 #define G3_PRESENCE_PORT                12346                               // UDP port
